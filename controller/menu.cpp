894662362d366590e3be5a8e5417901c2ae7483a
András Nemes <nemesa1@gmail.com>
2024-05-07 21:05:55 +0200

    refactor menu pt1

--- controller/menu.cpp
+++ controller/menu.cpp
@@ -11,6 +11,120 @@ public:
     dh.setup();
     eh.setup();
     wh.setup();
+    user = eh.getLastSelectedUser();
+    workout = user.lastSelection;
+    //setMenu(0, -1);
+    setMenu(1, 1);
+  }
+  void down() {
+    if (menu == 0) {
+      readNextUser();
+      dh.main(user.name);
+    }
+    if (menu == 1) {
+      subMenu = subMenu + 1;
+      if (subMenu == 4) {
+        subMenu = 1;
+      }
+      setMenu(1, subMenu);
+    }
+  }
+  void up() {
+    if (menu == 0) {
+      readPrevUser();
+      dh.main(user.name);
+    }
+    if (menu == 1) {
+      subMenu = subMenu - 1;
+      if (subMenu == 0) {
+        subMenu = 3;
+      }
+      setMenu(1, subMenu);
+    }
+  }
+  void left() {
+    if (menu == 1) {
+      setMenu(0, -1);
+    }
+  }
+  void right() {
+    if (menu == 1) {
+      setMenu(3, -1);
+    }
+  }
+  void select() {
+    if (menu == 0) {
+      eh.setLastUser(user.num);
+      setMenu(1, 1);
+    }
+  }
+
+private:
+  int menu = -1;
+  int subMenu = -1;
+  int workout = -1;
+  void render() {
+    if (menu == 0) {
+      dh.navigationOptions(true, false, true, false);
+      dh.header("Welcome!");
+      dh.main(user.name);
+    } else if (menu == 1) {
+      dh.navigationOptions(true, true, true, true);
+      dh.header("Select Workout!");
+      if (subMenu == 1) {
+        dh.mainSmallLine1("Start Last Selected:");
+        dh.mainSmallLine2(wh.getNameById(user.lastSelection));
+      } else if (subMenu == 2) {
+        dh.mainSmallLine1("Other Workouts");
+        dh.mainSmallLine2("");
+      } else if (subMenu == 3) {
+        dh.mainSmallLine1("User Settings");
+        dh.mainSmallLine2("");
+      }
+    } else if (menu == 3) {
+      /*dh.header("Last Workout Details:");
+      dh.navigationOptions(false, true, false, false);
+      dh.mainSmallLine1(wh.getNameById(user.lastSelection));
+      dh.mainSmallLine2(wh.getById(user.lastSelection));*/
+    }
+  };
+  void readNextUser() {
+    uint8_t num = user.num + 1;
+    if (num > 4) {
+      num = 1;
+    }
+    user = eh.readUser(num);
+  };
+  void readPrevUser() {
+    uint8_t num = user.num - 1;
+    if (num == 0) {
+      num = 4;
+    }
+    user = eh.readUser(num);
+  }
+  void setMenu(int newMenu, int newSubMenu) {
+    if (menu != newMenu) {
+      dh.cleanAll();
+    }
+    menu = newMenu;
+    subMenu = newSubMenu;
+    render();
+  };
+  UserDataStruct user;
+  DisplayHandler dh;
+  EEPROMHandler eh;
+  WorkoutHandler wh;
+};
+
+/*
+class MenuHandler {
+public:
+
+  void setup() {
+    Serial.println("MenuHandler-setup");
+    dh.setup();
+    eh.setup();
+    wh.setup();
 
     user = eh.getLastSelectedUser();
 
@@ -159,3 +273,4 @@ private:
   EEPROMHandler eh;
   WorkoutHandler wh;
 };
+*/
