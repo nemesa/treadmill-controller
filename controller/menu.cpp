1262c007ae8e3e5b8af27a0538eb51549086427a
András Nemes <nemesa1@gmail.com>
2024-05-07 22:44:33 +0200

    set speed

--- controller/menu.cpp
+++ controller/menu.cpp
@@ -12,10 +12,10 @@ public:
     eh.setup();
     wh.setup();
     user = eh.getLastSelectedUser();
-    workout = user.lastSelection;   
-    
-    setMenu(0, -1);
-    //setMenu(1, 2);
+    setUser(user.num);
+
+    //setMenu(0, -1);
+    setMenu(5, 1);
   }
   void down() {
     if (menu == 0) {
@@ -33,6 +33,24 @@ public:
         subMenu = 1;
       }
       setMenu(4, subMenu);
+    } else if (menu == 5 && subMenu < 10) {
+      subMenu = subMenu + 1;
+      if (subMenu == 4) {
+        subMenu = 1;
+      }
+      setMenu(5, subMenu);
+    } else if (menu == 5 && subMenu == 20) {
+      walkSpeed = walkSpeed - 1;
+      if (walkSpeed == 0) {
+        walkSpeed = 1;
+      }
+      dh.main(getSpeed(walkSpeed));
+    } else if (menu == 5 && subMenu == 30) {
+      runSpeed = runSpeed - 1;
+      if (runSpeed == 0) {
+        runSpeed = 1;
+      }
+      dh.main(getSpeed(runSpeed));
     }
   }
   void up() {
@@ -51,6 +69,24 @@ public:
         subMenu = 29;
       }
       setMenu(4, subMenu);
+    } else if (menu == 5 && subMenu < 10) {
+      subMenu = subMenu - 1;
+      if (subMenu == 0) {
+        subMenu = 3;
+      }
+      setMenu(5, subMenu);
+    } else if (menu == 5 && subMenu == 20) {
+      walkSpeed = walkSpeed + 1;
+      if (walkSpeed == 161) {
+        walkSpeed = 160;
+      }
+      dh.main(getSpeed(walkSpeed));
+    } else if (menu == 5 && subMenu == 30) {
+      runSpeed = runSpeed + 1;
+      if (runSpeed == 161) {
+        runSpeed = 160;
+      }
+      dh.main(getSpeed(runSpeed));
     }
   }
   void left() {
@@ -75,17 +111,35 @@ public:
       setMenu(1, 1);
     } else if (menu == 1 && subMenu == 2) {
       setMenu(4, user.lastSelection);
+    } else if (menu == 1 && subMenu == 3) {
+      setMenu(5, 1);
     } else if (menu == 4) {
       user.lastSelection = subMenu;
       eh.writeUserData(user);
-      setMenu(6, -1);  // TODO: should start workout here
+      setMenu(6, -1);
+    } else if (menu == 5) {
+      if (subMenu == 1) {
+      } else if (subMenu == 2) {
+        setMenu(5, 20);
+      } else if (subMenu == 3) {
+        setMenu(5, 30);
+      } else if (subMenu == 20) {
+        user.walkSpeed = walkSpeed;
+        eh.writeUserData(user);
+        setMenu(5, 2);
+      } else if (subMenu == 30) {
+        user.runSpeed = runSpeed;
+        eh.writeUserData(user);
+        setMenu(5, 3);
+      }
     }
   }
 
 private:
   int menu = -1;
   int subMenu = -1;
-  int workout = -1;
+  uint8_t walkSpeed = -1;
+  uint8_t runSpeed = -1;
   void render() {
     /*Serial.print(F("render "));
     Serial.print(menu);
@@ -128,10 +182,16 @@ private:
         dh.mainSmallLine2(user.name);
       } else if (subMenu == 2) {
         dh.mainSmallLine1("Walk Speed:");
-        dh.mainSmallLine2("");
+        dh.mainSmallLine2(getSpeed(user.walkSpeed));
+      } else if (subMenu == 20) {
+        dh.header("Walk Speed:");
+        dh.main(getSpeed(user.walkSpeed));
       } else if (subMenu == 3) {
         dh.mainSmallLine1("Run Speed:");
-        dh.mainSmallLine2("");
+        dh.mainSmallLine2(getSpeed(user.runSpeed));
+      } else if (subMenu == 30) {
+        dh.header("Run Speed:");
+        dh.main(getSpeed(user.runSpeed));
       }
     } else if (menu = 6) {
       dh.main("Workout!");
@@ -142,14 +202,14 @@ private:
     if (num > 4) {
       num = 1;
     }
-    user = eh.readUser(num);
+    setUser(num);
   };
   void readPrevUser() {
     uint8_t num = user.num - 1;
     if (num == 0) {
       num = 4;
     }
-    user = eh.readUser(num);
+    setUser(num);
   }
   void setMenu(int newMenu, int newSubMenu) {
     /*Serial.print(F("setMenu "));
@@ -163,6 +223,33 @@ private:
     subMenu = newSubMenu;
     render();
   };
+  char* getSpeed(uint8_t speed) {
+    Serial.print(F("getSpeed "));
+    Serial.println(speed);
+
+    char* result = "00.0";
+    if (speed < 100) {
+      result[0] = ' ';
+    }
+    if (speed < 10) {
+      result[3] = (char)(48 + speed);
+    } else {
+      if (speed > 100) {
+        result[0] = (char)(48 + speed / 100);
+        result[1] = (char)(48 + ((100-speed) / 10));
+        result[3] = (char)(48 + ((100-speed) % 10));
+      } else {
+        result[1] = (char)(48 + speed / 10);
+        result[3] = (char)(48 + speed % 10);
+      }
+    }
+    return result;
+  }
+  void setUser(uint8_t num) {
+    user = eh.readUser(num);
+    walkSpeed = user.walkSpeed;
+    runSpeed = user.runSpeed;
+  }
   UserDataStruct user;
   DisplayHandler dh;
   EEPROMHandler eh;
