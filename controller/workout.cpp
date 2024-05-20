8c5d26be6d197d7e654be89abfc6284f90214bf1
András Nemes <nemesa1@gmail.com>
2024-05-20 22:34:00 +0200

    optimalize workout handler pt2

--- controller/workout.cpp
+++ controller/workout.cpp
@@ -110,70 +110,418 @@ private:
   }
 
   char* get5KTraining(uint8_t week) {
-    char* result;
+    Serial.println(F("get5KTraining"));
+    char* result = "___________________";
+    Serial.println(result);
+    for (int i = 0; i < 19; i++) {
+      result[i] = (char)32;  //SPACE
+    }
+    Serial.println(result);
     if (week == 1 || week == 2) {
-      result = "2W|[5|1R|1W]";
+      //"2W|[5|1R|1W]"
+      result[0] = '2';
+      result[1] = 'W';
+      result[2] = '|';
+      result[3] = '[';
+      result[4] = '5';
+      result[5] = '|';
+      result[6] = '1';
+      result[7] = 'R';
+      result[8] = '|';
+      result[9] = '1';
+      result[10] = 'W';
+      result[11] = ']';
     } else if (week == 3 || week == 4) {
-      result = "2W|[8|1R|1W]|1W";
-    } else if (week == 4) {
-      result = "2W|[8|1R|1W]|1W";
+      //"2W|[8|1R|1W]|1W"
+      result[0] = '2';
+      result[1] = 'W';
+      result[2] = '|';
+      result[3] = '[';
+      result[4] = '8';
+      result[5] = '|';
+      result[6] = '1';
+      result[7] = 'R';
+      result[8] = '|';
+      result[9] = '1';
+      result[10] = 'W';
+      result[11] = ']';
+      result[12] = '|';
+      result[13] = '1';
+      result[14] = 'W';
     } else if (week == 5) {
-      result = "3W|[4|2R|2W]|1W";
+      //3W|[4|2R|2W]|1W"
+      result[0] = '3';
+      result[1] = 'W';
+      result[2] = '|';
+      result[3] = '[';
+      result[4] = '4';
+      result[5] = '|';
+      result[6] = '2';
+      result[7] = 'R';
+      result[8] = '|';
+      result[9] = '2';
+      result[10] = 'W';
+      result[11] = ']';
+      result[12] = '|';
+      result[13] = '1';
+      result[14] = 'W';
+
     } else if (week == 6) {
-      result = "3W|[5|2R|1W]|1W";
+      //"3W|[5|2R|1W]|1W"
+      result[0] = '3';
+      result[1] = 'W';
+      result[2] = '|';
+      result[3] = '[';
+      result[4] = '5';
+      result[5] = '|';
+      result[6] = '2';
+      result[7] = 'R';
+      result[8] = '|';
+      result[9] = '1';
+      result[10] = 'W';
+      result[11] = ']';
+      result[12] = '|';
+      result[13] = '1';
+      result[14] = 'W';
     } else if (week == 7) {
-      result = "4W|[2|2R|1W|3R|2W]";
+      //"4W|[2|2R|1W|3R|2W]"
+      result[0] = '4';
+      result[1] = 'W';
+      result[2] = '|';
+      result[3] = '[';
+      result[4] = '2';
+      result[5] = '|';
+      result[6] = '2';
+      result[7] = 'R';
+      result[8] = '|';
+      result[9] = '1';
+      result[10] = 'W';
+      result[11] = '|';
+      result[12] = '3';
+      result[13] = 'R';
+      result[14] = ']';
+      result[15] = '|';
+      result[16] = '2';
+      result[17] = 'W';
     } else if (week == 8) {
-      result = "4W|[3|3R|2W]|3W";
+      //"4W|[3|3R|2W]|3W"
+      result[0] = '4';
+      result[1] = 'W';
+      result[2] = '|';
+      result[3] = '[';
+      result[4] = '3';
+      result[5] = '|';
+      result[6] = '3';
+      result[7] = 'R';
+      result[8] = '|';
+      result[9] = '2';
+      result[10] = 'W';
+      result[11] = ']';
+      result[12] = '|';
+      result[13] = '3';
+      result[14] = 'W';
     } else if (week == 9) {
-      result = "4W|[4|3R|1W]|3W";
+      //"4W|[4|3R|1W]|3W"
+      result[0] = '4';
+      result[1] = 'W';
+      result[2] = '|';
+      result[3] = '[';
+      result[4] = '4';
+      result[5] = '|';
+      result[6] = '3';
+      result[7] = 'R';
+      result[8] = '|';
+      result[9] = '1';
+      result[10] = 'W';
+      result[11] = ']';
+      result[12] = '|';
+      result[13] = '3';
+      result[14] = 'W';
     } else if (week == 10) {
-      result = "5W|[3|4R|2W]|2W";
+      //"5W|[3|4R|2W]|2W"
+      result[0] = '5';
+      result[1] = 'W';
+      result[2] = '|';
+      result[3] = '[';
+      result[4] = '3';
+      result[5] = '|';
+      result[6] = '4';
+      result[7] = 'R';
+      result[8] = '|';
+      result[9] = '2';
+      result[10] = 'W';
+      result[11] = ']';
+      result[12] = '|';
+      result[13] = '2';
+      result[14] = 'W';
     } else if (week == 11) {
-      result = "5W|[2|5R|2W]|3R|4W";
+      //"5W|[2|5R|2W]|3R|4W"
+      result[0] = '5';
+      result[1] = 'W';
+      result[2] = '|';
+      result[3] = '[';
+      result[4] = '2';
+      result[5] = '|';
+      result[6] = '5';
+      result[7] = 'R';
+      result[8] = '|';
+      result[9] = '2';
+      result[10] = 'W';
+      result[11] = '|';
+      result[12] = '3';
+      result[13] = 'R';
+      result[14] = ']';
+      result[15] = '|';
+      result[16] = '4';
+      result[17] = 'W';
     } else if (week == 12) {
-      result = "5W|[2|7R|2W]|2W";
+      //"5W|[2|7R|2W]|2W"
+      result[0] = '5';
+      result[1] = 'W';
+      result[2] = '|';
+      result[3] = '[';
+      result[4] = '2';
+      result[5] = '|';
+      result[6] = '7';
+      result[7] = 'R';
+      result[8] = '|';
+      result[9] = '2';
+      result[10] = 'W';
+      result[11] = ']';
+      result[12] = '|';
+      result[13] = '2';
+      result[14] = 'W';
     } else if (week == 13) {
-      result = "5W|10R|2W|5R|5W";
+      //"5W|10R|2W|5R|5W"
+      result[0] = '5';
+      result[1] = 'W';
+      result[2] = '|';
+      result[3] = '1';
+      result[4] = '0';
+      result[5] = 'R';
+      result[6] = '|';
+      result[7] = '2';
+      result[8] = 'W';
+      result[9] = '|';
+      result[10] = '5';
+      result[11] = 'R';
+      result[12] = '|';
+      result[13] = '5';
+      result[14] = 'W';
     } else if (week == 14) {
-      result = "5W|15R|6W";
+      //"5W|15R|6W"
+      result[0] = '5';
+      result[1] = 'W';
+      result[2] = '|';
+      result[3] = '1';
+      result[4] = '5';
+      result[5] = 'R';
+      result[6] = '|';
+      result[7] = '6';
+      result[8] = 'W';
     } else if (week == 15) {
-      result = "5W|[4|5R|1W]|2W";
+      //"5W|[4|5R|1W]|2W"
+      result[0] = '5';
+      result[1] = 'W';
+      result[2] = '|';
+      result[3] = '[';
+      result[4] = '4';
+      result[5] = '|';
+      result[6] = '5';
+      result[7] = 'R';
+      result[8] = '|';
+      result[9] = '1';
+      result[10] = 'W';
+      result[11] = ']';
+      result[12] = '|';
+      result[13] = '2';
+      result[14] = 'W';
     } else if (week == 16) {
-      result = "5W|[3|6R|1W]|4W";
+      //"5W|[3|6R|1W]|4W"
+      result[0] = '5';
+      result[1] = 'W';
+      result[2] = '|';
+      result[3] = '[';
+      result[4] = '3';
+      result[5] = '|';
+      result[6] = '6';
+      result[7] = 'R';
+      result[8] = '|';
+      result[9] = '1';
+      result[10] = 'W';
+      result[11] = ']';
+      result[12] = '|';
+      result[13] = '4';
+      result[14] = 'W';
     } else if (week == 17) {
-      result = "5W|[3|7R|2W]|4W";
+      //"5W|[3|7R|2W]|4W"
+      result[0] = '5';
+      result[1] = 'W';
+      result[2] = '|';
+      result[3] = '[';
+      result[4] = '3';
+      result[5] = '|';
+      result[6] = '7';
+      result[7] = 'R';
+      result[8] = '|';
+      result[9] = '2';
+      result[10] = 'W';
+      result[11] = ']';
+      result[12] = '|';
+      result[13] = '4';
+      result[14] = 'W';
     } else if (week == 18) {
-      result = "5W|10R|2W|12R|4W";
+      //"5W|10R|2W|12R|4W"
+      result[0] = '5';
+      result[1] = 'W';
+      result[2] = '|';
+      result[3] = '1';
+      result[4] = '0';
+      result[5] = 'R';
+      result[6] = '|';
+      result[7] = '2';
+      result[8] = 'W';
+      result[9] = '|';
+      result[10] = '1';
+      result[11] = '2';
+      result[12] = 'R';
+      result[13] = '|';
+      result[14] = '4';
+      result[15] = 'W';
     } else if (week == 19) {
-      result = "5W|[3|9R|1W]";
+      //"5W|[3|9R|1W]"
+      result[0] = '5';
+      result[1] = 'W';
+      result[2] = '|';
+      result[3] = '[';
+      result[4] = '3';
+      result[5] = '|';
+      result[6] = '9';
+      result[7] = 'R';
+      result[8] = '|';
+      result[9] = '1';
+      result[10] = 'W';
+      result[11] = ']';
     } else if (week == 20) {
-      result = "5W|15R|2W|[2|5R|2W]";
+      //"5W|15R|2W|[2|5R|2W]"
+      result[0] = '5';
+      result[1] = 'W';
+      result[3] = '|';
+      result[4] = '1';
+      result[5] = '5';
+      result[6] = 'R';
+      result[7] = '|';
+      result[8] = '2';
+      result[9] = 'W';
+      result[10] = '|';
+      result[11] = '[';
+      result[12] = '2';
+      result[13] = '|';
+      result[14] = '5';
+      result[15] = 'R';
+      result[16] = '|';
+      result[17] = '2';
+      result[18] = 'W';
+      result[19] = ']';
     } else if (week == 21) {
-      result = "5W|[3|9R|2W]";
+      //"5W|[3|9R|2W]"
+      result[0] = '5';
+      result[1] = 'W';
+      result[2] = '|';
+      result[3] = '[';
+      result[4] = '3';
+      result[5] = '|';
+      result[6] = '9';
+      result[7] = 'R';
+      result[8] = '|';
+      result[9] = '2';
+      result[10] = 'W';
+      result[11] = ']';
     } else if (week == 22) {
-      result = "5W|[3|12R|2W]|4R|4W";
+      //"5W|[3|12R|2W]|4R|4W"
+      result[0] = '5';
+      result[1] = 'W';
+      result[2] = '|';
+      result[3] = '[';
+      result[4] = '3';
+      result[5] = '|';
+      result[6] = '1';
+      result[7] = '2';
+      result[8] = 'R';
+      result[9] = '|';
+      result[10] = '2';
+      result[11] = 'W';
+      result[12] = ']';
+      result[13] = '|';
+      result[14] = '4';
+      result[15] = 'R';
+      result[16] = '|';
+      result[17] = '4';
+      result[18] = 'W';
     } else if (week == 23) {
-      result = "5W|[3|14R|2W]|2W";
+      //"5W|[3|14R|2W]|2W"
+      result[0] = '5';
+      result[1] = 'W';
+      result[2] = '|';
+      result[3] = '[';
+      result[4] = '3';
+      result[5] = '|';
+      result[6] = '1';
+      result[7] = '4';
+      result[8] = 'R';
+      result[9] = '|';
+      result[10] = '2';
+      result[11] = 'W';
+      result[12] = ']';
+      result[13] = '|';
+      result[14] = '2';
+      result[15] = 'W';
     } else if (week == 24) {
-      result = "5W|18R|2W|10R|4W";
+      //"5W|18R|2W|10R|4W"
+      result[0] = '5';
+      result[1] = 'W';
+      result[2] = '|';
+      result[3] = '1';
+      result[4] = '8';
+      result[5] = 'R';
+      result[6] = '|';
+      result[7] = '2';
+      result[8] = 'W';
+      result[9] = '|';
+      result[11] = '1';
+      result[12] = '0';
+      result[13] = 'R';
+      result[14] = '|';
+      result[15] = '4';
+      result[16] = 'W';
     } else {
       result = "";
     }
+    Serial.println(result);
     return result;
   }
 
   char* otherName(uint8_t id) {
+    char* a = "30 Min Walk";
+    char* b = "1 Hour 15 Min Walk";
     if (id == 25) {
-      return "30 Min Walk";
+      a[0] = '3';
+      a[1] = '0';
+      return a;
     } else if (id == 26) {
-      return "45 Min Walk";
+      a[0] = '4';
+      a[1] = '5';
+      return a;
     } else if (id == 27) {
       return "1 Hour Walk";
     } else if (id == 28) {
-      return "1 Hour 15 Min Walk";
+      b[7] = '1';
+      b[8] = '5';
+      return b;
     } else if (id == 29) {
-      return "1 Hour 30 Min Walk";
+      b[7] = '3';
+      b[8] = '0';
+      return b;
     } else {
       return "INVALID";
     }
